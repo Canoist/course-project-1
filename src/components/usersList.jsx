178299bfa3f52efefdb770173c8a0b2582200700
Leonid Lichkun <lionproklon@gmail.com>
2021-12-02 16:57:23 +0300

    Realized header

--- src/components/usersList.jsx
+++ src/components/usersList.jsx
@@ -5,14 +5,27 @@ import Table from "./table";
 
 const UsersList = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
-  const removeUser = (user) => {
+  const handleDelete = (user) => {
     setUsers(users.filter((p) => p._id !== user._id));
   };
-  console.log(users);
+  const renderPhrase = (number) => {
+    return number ? (
+      <h2>
+        <span class="badge bg-primary">
+          {number} человек тусанет с тобой сегодня
+        </span>
+      </h2>
+    ) : (
+      <h2>
+        <span class="badge bg-danger">Никто не тусанет с тобой сегодня</span>
+      </h2>
+    );
+  };
 
   return (
     <div>
-      <Table users={users} remove={removeUser} />
+      {renderPhrase(users.length)}
+      <Table users={users} remove={handleDelete} />
     </div>
   );
 };
