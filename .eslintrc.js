a7bb99684752b995abdf044d6a4b0b4d15ba9aba
Leonid Lichkun <lionproklon@gmail.com>
2021-12-12 01:46:26 +0300

    Changing ESLinnt.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
+  extends: ["plugin:react/recommended", "standard"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-  }
-}
+    semi: [2, "always"],
+    "space-before-function-paren": ["error", "always"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+  },
+};
