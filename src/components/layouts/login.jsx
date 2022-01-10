cd008e24a94059e6b9e8e772bc72125456178181
Leonid Lichkun <lionproklon@gmail.com>
2022-01-10 23:21:41 +0300

    make form with input and label

--- src/components/layouts/login.jsx
+++ src/components/layouts/login.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [email, setEmail] = useState();
+  const handleChange = ({ target }) => {
+    setEmail(target.value);
+  };
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" value={email} onChange={handleChange} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </div>
+    </form>
+  );
 };
 export default Login;
