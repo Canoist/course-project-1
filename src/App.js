c868fb6e6761747fc26e809960c3a58d27795943
Leonid Lichkun <lionproklon@gmail.com>
2022-02-06 01:01:32 +0300

    Small refactor, added UseUsers in ./hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Login from "../layouts/login";
-import Main from "../layouts/main";
-import Users from "../layouts/users";
-import NavBar from "../components/ui/navBar";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
+import Users from "./layouts/users";
+import NavBar from "./components/ui/navBar";
 
 const App = () => {
   return (
