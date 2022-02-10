784bb3a1bbc7c4b01d93a4148e2614bac67505ff
Leonid Lichkun <lionproklon@gmail.com>
2022-02-10 19:45:08 +0300

    Refactor of put/get data from FireBase

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,6 +1,25 @@
 import React from "react";
+import useMockData from "../utils/mockData";
 
 const Main = () => {
-  return <h1>Main Page</h1>;
+  const { error, initialize, progress, status } = useMockData();
+
+  return (
+    <div className="container mt-5">
+      <h1>Main Page</h1>
+      <h3>Initialisation data at FireBase</h3>
+      <button
+        className={"btn btn-primary " + (status === "Ready" && "disabled")}
+        onClick={initialize}
+      >
+        Initializing
+      </button>
+      <ul>
+        <li>Status: {status}</li>
+        <li>Progress: {progress}</li>
+        {error && <li>Error: {error}</li>}
+      </ul>
+    </div>
+  );
 };
 export default Main;
