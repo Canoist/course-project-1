d483774e9c75697a36601b26e4119ea4d535d5a0
Leonid Lichkun <lionproklon@gmail.com>
2022-02-14 18:11:46 +0300

    Changed rules in FireBase, making to get access to content for only auth users

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -28,6 +28,10 @@ http.interceptors.request.use(
           localId: data.local_id
         });
       }
+      const accessToken = localStorageService.getAccessToken();
+      if (accessToken) {
+        config.params = { ...config.params, auth: accessToken };
+      }
     }
 
     return config;
