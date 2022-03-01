4da6123d8d0c04c477f9313b6db106b39caf2d27
Leonid Lichkun <lionproklon@gmail.com>
2022-03-01 22:12:29 +0300

    Created slices and basic reducers for qualities

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "./store/createStore";
+import { Provider } from "react-redux";
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
